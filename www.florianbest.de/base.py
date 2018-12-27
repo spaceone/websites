e320e29c3abcf87780355d25eaa5dfd060168e58
spaceone <space@wechall.net>
2018-12-27 23:56:27 +0100

    Allow ressources to overwrite global config

--- www.florianbest.de/base.py
+++ www.florianbest.de/base.py
@@ -18,6 +18,13 @@ from circuits.http.server.i18n.content_language import ContentLanguage
 from .config import config
 
 
+class websiteproperty(property):
+
+	def __init__(self, fget, *a, **kw):
+		self.name = kw.pop('name', fget.__name__.replace('website_', ''))
+		property.__init__(self, fget, *a, **kw)
+
+
 class _Resource(GettextResource, BaseResource):
 
 	default_features = BaseResource.default_features + [ContentLanguage]
@@ -77,6 +84,13 @@ class _Resource(GettextResource, BaseResource):
 class Resource(_Resource):
 
 	loaders = dict()
+	config = dict()
+
+	def __init__(self, *args, **kwargs):
+		super(Resource, self).__init__(*args, **kwargs)
+		if not Resource.config:
+			Resource.config = config()
+		self.config = Resource.config.copy()
 
 	def load(self, path):
 		if path not in self.loaders:
@@ -98,6 +112,7 @@ class Resource(_Resource):
 			content = dict(content=content)
 		content.update(dict(
 			user=client.user,
+			client=client,
 		))
 		return content
 
@@ -132,6 +147,18 @@ class Resource(_Resource):
 	def website_template_name(self):
 		return 'website.tpl'
 
+	@websiteproperty
+	def website_meta(self):
+		return self.config.get('meta', [])
+
+	@websiteproperty
+	def website_scripts(self):
+		return self.config.get('scripts', [])
+
+	@websiteproperty
+	def website_links(self):
+		return self.config.get('links', [])
+
 	def website_tplvars(self, client):
 		module = type(self).__module__.split('.', 1)[0]
 		source = sys.modules[type(self).__module__].__file__.split(module, 1)[-1]
@@ -142,9 +169,9 @@ class Resource(_Resource):
 			_=lambda x: x,
 			source=source,
 		)
-		tplvars.update(config())
-#		for iname, prop in inspect.getmembers(self.__class__, lambda prop: isinstance(prop, websiteproperty)):
-#			tplvars[prop.name] = getattr(self, iname)
+		tplvars.update(self.config)
+		for iname, prop in inspect.getmembers(self.__class__, lambda prop: isinstance(prop, websiteproperty)):
+			tplvars[prop.name] = getattr(self, iname)
 		return tplvars
 
 	@httphandler('request', priority=0.45)

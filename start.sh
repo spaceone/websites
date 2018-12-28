6e64b54d09ecf1fd1bb408bbe85382d9e84038f9
spaceone <space@wechall.net>
2018-12-28 02:05:42 +0100

    Add script to start the server

---
+++ start.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python www.py --bind http://0.0.0.0:8090/ --bind 'https://0.0.0.0:8443/?certfile=/etc/letsencrypt/live/www.florianbest.de/cert.pem&keyfile=/etc/letsencrypt/live/www.florianbest.de/privkey.pem' -c domains.cfg -n -l /dev/stderr $@
